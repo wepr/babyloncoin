35617e8ef9870234843da0226f22a313aadf1955
aivve <maksymivv@gmail.com>
2018-04-04 00:04:44 +0300

    Min. fee tx validation

--- src/CryptoNoteCore/Core.cpp
+++ src/CryptoNoteCore/Core.cpp
@@ -244,6 +244,35 @@ bool core::check_tx_mixin(const Transaction& tx) {
   return true;
 }
 
+bool core::check_tx_fee(const Transaction& tx, size_t blobSize, tx_verification_context& tvc) {
+	uint64_t inputs_amount = 0;
+	if (!get_inputs_money_amount(tx, inputs_amount)) {
+		tvc.m_verifivation_failed = true;
+		return false;
+	}
+
+	uint64_t outputs_amount = get_outs_money_amount(tx);
+
+	if (outputs_amount > inputs_amount) {
+		logger(INFO) << "transaction use more money then it has: use " << m_currency.formatAmount(outputs_amount) <<
+			", have " << m_currency.formatAmount(inputs_amount);
+		tvc.m_verifivation_failed = true;
+		return false;
+	}
+
+	Crypto::Hash h = NULL_HASH;
+	getObjectHash(tx, h, blobSize);
+	const uint64_t fee = inputs_amount - outputs_amount;
+	bool isFusionTransaction = fee == 0 && m_currency.isFusionTransaction(tx, blobSize);
+	if (!isFusionTransaction && fee < m_currency.minimumFee()) {
+		logger(INFO) << "transaction fee is not enough: " << m_currency.formatAmount(fee) <<
+			", minimum fee: " << m_currency.formatAmount(m_currency.minimumFee());
+		tvc.m_verifivation_failed = true;
+		tvc.m_tx_fee_too_small = true;
+		return false;
+	}
+}
+
 bool core::check_tx_semantic(const Transaction& tx, bool keeped_by_block) {
   if (!tx.inputs.size()) {
     logger(ERROR) << "tx with empty inputs, rejected for tx id= " << getObjectHash(tx);
@@ -1019,6 +1048,11 @@ bool core::handleIncomingTransaction(const Transaction& tx, const Crypto::Hash& 
     logger(INFO) << "WRONG TRANSACTION BLOB, Failed to check tx " << txHash << " syntax, rejected";
     tvc.m_verifivation_failed = true;
     return false;
+  }
+
+  if (!check_tx_fee(tx, blobSize, tvc)) {
+	  tvc.m_verifivation_failed = true;
+	  return false;
   }
 
   if (!check_tx_mixin(tx)) {
