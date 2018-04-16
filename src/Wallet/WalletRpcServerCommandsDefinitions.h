f1a822928204229d11986c42ba8f94a40908f2e7
aivve <maksymivv@gmail.com>
2018-04-17 01:54:08 +0300

    Default minimum fee if not set in rpc transfer method

--- src/Wallet/WalletRpcServerCommandsDefinitions.h
+++ src/Wallet/WalletRpcServerCommandsDefinitions.h
@@ -71,6 +71,10 @@ using CryptoNote::ISerializer;
 			uint64_t unlock_time;
 			std::string payment_id;
 
+			request() :fee(0),
+					   mixin(0),
+					   unlock_time(0){}
+
 			void serialize(ISerializer& s)
 			{
 				KV_MEMBER(destinations)
